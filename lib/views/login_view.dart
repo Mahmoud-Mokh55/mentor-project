72f6dac5119b0f2bb653b4742715b4aadff1ec42
mokh <mahmouddado555@outlook.com>
2024-08-24 19:45:45 +0300

    ..

--- lib/views/login_view.dart
+++ lib/views/login_view.dart
@@ -15,85 +15,87 @@ class LoginView extends StatelessWidget {
       body: SafeArea(
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 32),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              SizedBox(
-                height: MediaQuery.of(context).size.height * 0.15,
-              ),
-              const Text(
-                'Welcome Back!',
-                style: TextStyle(
-                  fontSize: 30,
-                  fontWeight: FontWeight.w800,
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(
+                  height: MediaQuery.of(context).size.height * 0.15,
                 ),
-              ),
-              const Text(
-                'Please Enter Your Login Details Below',
-                style: TextStyle(
-                  fontSize: 12,
-                  color: Colors.black54,
-                  fontWeight: FontWeight.w400,
-                ),
-              ),
-              SizedBox(
-                height: MediaQuery.of(context).size.height * 0.07,
-              ),
-              CustomTextField(
-                controller: passwordController,
-                textLabel: 'E-Mail',
-                isPassword: false,
-              ),
-              SizedBox(
-                height: MediaQuery.of(context).size.height * 0.03,
-              ),
-              CustomTextField(
-                controller: emailController,
-                textLabel: 'Password',
-                isPassword: true,
-              ),
-              SizedBox(
-                height: MediaQuery.of(context).size.height * 0.012,
-              ),
-              TextButton(
-                onPressed: () {},
-                child: const Text(
-                  'Forgot Password?',
-                  style: TextStyle(color: Colors.blue),
-                ),
-              ),
-              SizedBox(
-                height: MediaQuery.of(context).size.height * 0.045,
-              ),
-              const LoginButton(),
-              SizedBox(
-                height: MediaQuery.of(context).size.height * 0.07,
-              ),
-              const Row(
-                children: [
-                  Expanded(child: Divider(color: Color(0xFFB7B7B7))),
-                  Text(
-                    ' Or Better Yet... ',
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      color: Color(0xFFB7B7B7),
-                      fontSize: 15,
-                      fontFamily: 'Lato',
-                      fontWeight: FontWeight.w300,
-                      height: 0,
-                    ),
+                const Text(
+                  'Welcome Back!',
+                  style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.w800,
                   ),
-                  Expanded(child: Divider(color: Color(0xFFB7B7B7))),
-                ],
-              ),
-              SizedBox(
-                height: MediaQuery.of(context).size.height * 0.07,
-              ),
-              const CustomLoginOption(),
-              SizedBox(
-                height: MediaQuery.of(context).size.height * 0.07,
-              ),
-            ],
+                ),
+                const Text(
+                  'Please Enter Your Login Details Below',
+                  style: TextStyle(
+                    fontSize: 12,
+                    color: Colors.black54,
+                    fontWeight: FontWeight.w400,
+                  ),
+                ),
+                SizedBox(
+                  height: MediaQuery.of(context).size.height * 0.07,
+                ),
+                CustomTextField(
+                  controller: passwordController,
+                  textLabel: 'E-Mail',
+                  isPassword: false,
+                ),
+                SizedBox(
+                  height: MediaQuery.of(context).size.height * 0.03,
+                ),
+                CustomTextField(
+                  controller: emailController,
+                  textLabel: 'Password',
+                  isPassword: true,
+                ),
+                SizedBox(
+                  height: MediaQuery.of(context).size.height * 0.012,
+                ),
+                TextButton(
+                  onPressed: () {},
+                  child: const Text(
+                    'Forgot Password?',
+                    style: TextStyle(color: Colors.blue),
+                  ),
+                ),
+                SizedBox(
+                  height: MediaQuery.of(context).size.height * 0.045,
+                ),
+                const LoginButton(),
+                SizedBox(
+                  height: MediaQuery.of(context).size.height * 0.07,
+                ),
+                const Row(
+                  children: [
+                    Expanded(child: Divider(color: Color(0xFFB7B7B7))),
+                    Text(
+                      ' Or Better Yet... ',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        color: Color(0xFFB7B7B7),
+                        fontSize: 15,
+                        fontFamily: 'Lato',
+                        fontWeight: FontWeight.w300,
+                        height: 0,
+                      ),
+                    ),
+                    Expanded(child: Divider(color: Color(0xFFB7B7B7))),
+                  ],
+                ),
+                SizedBox(
+                  height: MediaQuery.of(context).size.height * 0.07,
+                ),
+                const CustomLoginOption(),
+                SizedBox(
+                  height: MediaQuery.of(context).size.height * 0.07,
+                ),
+              ],
+            ),
           ),
         ),
       ),
